4e32c91b4bc8625122083165f3438c461ff06035
fuerbringer <severin@protonmail.ch>
2018-10-22 22:41:26 +0200

    Add Babel to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     filename: 'bundle.js',
     libraryTarget: 'var',
     library: 'bma'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
